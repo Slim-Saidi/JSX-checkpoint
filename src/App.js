8ade08f22ff3b822ba971c224fb3032a22492689
slim-saidi <slim-saidi@hotmail.com>
2020-11-25 15:26:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './styles.css'
+import mypic from "./jeeptrackhawk.jpg"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="first">
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="title red">Slim</h1>
+      <br />
+      <img src={mypic} style={{maxWidth: '400px'}} alt= 'mypic'/>
+      <br />
+      <img src="./dodge.jpg" style={{maxWidth: '400px'}} />
+    </div>
+    <video width={320} height={240} controls >
+    <iframe
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}
+        src={`https://www.youtube.com/watch?v=GYAiOuE56R8`}
+        frameBorder="0"
+      />
+    </video>
     </div>
   );
 }
